c55a687d248cf7856e189928b1872b79957e6b8a
Victor Radermecker <victor.radermecker@gmail.com>
2023-04-17 13:42:25 -0400

    Mean Pixel Extracted

--- src/fishnet.py
+++ src/fishnet.py
@@ -3,6 +3,8 @@ from shapely.geometry import Polygon
 from pyproj import Geod
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import pickle
+from shapely.geometry import MultiPolygon
 
 
 class Fishnet:
@@ -34,13 +36,13 @@ class Fishnet:
         xmin, ymin, xmax, ymax = self.tx.total_bounds
         x_size = xmax - xmin
         y_size = ymax - ymin
-        num_cols = round(x_size / tile_size)
-        num_rows = round(y_size / tile_size)
+        self.num_cols = round(x_size / tile_size)
+        self.num_rows = round(y_size / tile_size)
 
         # Create the fishnet polygons
         fishnet_polys = []
-        for i in tqdm(range(num_rows)):
-            for j in range(num_cols):
+        for i in tqdm(range(self.num_rows)):
+            for j in range(self.num_cols):
                 # Calculate the coordinates of the fishnet cell corners
                 x_left = xmin + j * tile_size
                 x_right = xmin + (j + 1) * tile_size
@@ -115,3 +117,77 @@ class Fishnet:
         filtered_fishnet = self.fishnet[self.fishnet.intersects(bounding_box)]
 
         return filtered_fishnet
+
+    def save(self, file_path):
+        """
+        Save the Fishnet object to a file using pickle.
+
+        Parameters:
+        file_path (str): The file path to save the Fishnet object.
+        """
+        with open(file_path, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def load(cls, file_path):
+        """
+        Load a Fishnet object from a file using pickle.
+
+        Parameters:
+        file_path (str): The file path of the saved Fishnet object.
+
+        Returns:
+        Fishnet: The loaded Fishnet object.
+        """
+        with open(file_path, "rb") as file:
+            return pickle.load(file)
+
+    def batch(self, ncols, nrows):
+        """
+        Group the fishnet tiles into rectangular batches of the same size.
+
+        Parameters:
+        ncols (int): The number of columns in each batch.
+        nrows (int): The number of rows in each batch.
+
+        Returns:
+        GeoDataFrame: A new GeoDataFrame with a less granular fishnet containing all the previous smaller tiles.
+        """
+        # Calculate the number of original fishnet rows and columns
+        xmin, ymin, xmax, ymax = self.fishnet.total_bounds
+        x_size = xmax - xmin
+        y_size = ymax - ymin
+
+        # Check if ncols and nrows are valid factors of the original fishnet's dimensions
+        if self.num_cols % ncols != 0 or self.num_rows % nrows != 0:
+            raise ValueError(
+                "The provided ncols and nrows are not valid factors for the original fishnet's dimensions."
+            )
+
+        # Calculate the number of batch rows and columns
+        num_batch_cols = self.num_cols // ncols
+        num_batch_rows = self.num_rows // nrows
+
+        # Initialize a list of empty MultiPolygons for each batch cell
+        batch_polys = [MultiPolygon() for _ in range(num_batch_rows * num_batch_cols)]
+
+        # Iterate through the smaller tiles in the fishnet
+        for tile in tqdm(self.fishnet.itertuples()):
+            # Calculate the batch cell indices corresponding to the current tile
+            tile_xmin, tile_ymin, tile_xmax, tile_ymax = tile.geometry.bounds
+            col_idx = int((tile_xmin - xmin) // (self.tile_size_miles * ncols))
+            row_idx = int((tile_ymin - ymin) // (self.tile_size_miles * nrows))
+
+            # Calculate the batch cell index in the batch_polys list
+            batch_idx = row_idx * num_batch_cols + col_idx
+
+            # Update the MultiPolygon for the corresponding batch cell with the current tile
+            batch_polys[batch_idx] = batch_polys[batch_idx].union(tile.geometry)
+
+        # Create a GeoDataFrame from the batch polygons
+        batch_gdf = gpd.GeoDataFrame(
+            {"id": range(len(batch_polys)), "geometry": batch_polys},
+            crs=self.fishnet.crs,
+        )
+
+        return batch_gdf
