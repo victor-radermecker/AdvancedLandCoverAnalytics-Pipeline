2cca965473be24fead0f8175cda96740e64d8b15
Victor Radermecker <victor.radermecker@gmail.com>
2023-04-26 13:46:52 -0400

    Reparing Fishnet: Harvesine Formula - Rectangles into Squares

--- src/ImageExporter.py
+++ src/ImageExporter.py
@@ -6,11 +6,19 @@ ee.Initialize()
 
 
 class ImageExporter:
-    def __init__(self, fishnet):
-        self.fishnet = fishnet
-        self.scale = 10  # resolution in meters per pixel
+    def __init__(self, fishnet, filtered, scale=10):
+        self.fh = fishnet
+        self.scale = scale
         self.fileFormat = "GeoTIFF"
         self.maxPixels = 1e10
+        self.filtered = filtered
+
+        if self.filtered:
+            self.fishnet = self.fh.filtered_fishnet
+            self.batches = self.fh.filtered_batches
+        else:
+            self.fishnet = self.fh.fishnet
+            self.batches = self.fh.batches
 
     def set_date_range(self, year):
         startDate = f"{year}-05-01"
@@ -20,11 +28,12 @@ class ImageExporter:
         self.endDate = endDate
 
     def set_folder(self, folder):
-        self.folder = folder  # Google Drive Folder that the export will reside in
+        # Google Drive Folder that the export will reside in
+        self.folder = folder
 
     def export_images(self, region):
-        for i in tqdm(range(len(self.fishnet_creator.filtered_batches))):
-            batch = self.fishnet_creator.filtered_batches.iloc[i]
+        for i in tqdm(range(len(self.batches))):
+            batch = self.batches.iloc[i]
             batch_region = ee.Geometry.Rectangle(batch["geometry"].bounds)
             intersection = region.intersection(batch_region)
             if intersection.is_empty().getInfo():
@@ -34,7 +43,7 @@ class ImageExporter:
                 intersection, self.startDate, self.endDate, return_type="visualize"
             )
 
-            ### Save the image
+            # Save the image
             export_params = {
                 "image": landcover,
                 "description": f'landcover_batchID_{batch["batch_id"]}',
