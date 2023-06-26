e79fbab450d905f690fed9b75c3e887a687bed86
Andrea <azanon@mit.edu>
2023-06-26 16:50:51 -0400

    improved feature engineering

--- src/XGB.py
+++ src/XGB.py
@@ -12,30 +12,17 @@ class XGB:
       self.df = deepcopy(fishnet.filtered_fishnet)
     else:
       self.df = deepcopy(fishnet.fishnet)
-
-  ################################
-  # CHECK AND SEE IF CAN IMPROVE #
-  ################################
     
   def feature_engineering(self, yearStart, yearEnd):
     self.years_range = range(yearStart, yearEnd + 1)
     for yr in self.years_range[:-1]:
       for feature in ["MeanPixel", "Entropy"]:
-        if feature == "Entropy":
-          if yr == self.years_range[-1]:
+        if feature == "Entropy" and yr == self.years_range[-1]:
             continue
-
-        yr_feature = np.array(self.df[f"{feature}_{yr}"])
-        next_yr_feature = np.array(self.df[f"{feature}_{yr+1}"])
-        self.df[f"Δ{feature}_{yr}_{yr+1}"] = next_yr_feature - yr_feature
-        # XGB already non-linear
-        # self.df[f"Delta2_{feature}_{yr}_{yr+1}"] = (next_yr_feature - yr_feature) / (next_yr_feature + yr_feature)
+        self.df[f"Δ{feature}_{yr}_{yr+1}"] = self.df[f"{feature}_{yr}"] - self.df[f"{feature}_{yr+1}"]
 
   def remove_original_features(self):
-    to_drop = []
-    for yr in self.years_range:
-      for feature in ["MeanPixel", "Entropy"]:
-        to_drop.append(f"{feature}_{yr}")
+    to_drop = [f"{feature}_{yr}" for yr in self.years_range for feature in ["MeanPixel", "Entropy"]]
     self.df.drop(columns = to_drop, axis = 1, inplace = True)
 
 
