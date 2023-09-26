8699d183194a30fd510ba366a865295b1d679331
Ubuntu <ubuntu@ip-10-0-9-204.us-east-2.compute.internal>
2023-09-26 15:30:25 +0000

    adding new urban code

--- src/ImageDataExporter.py
+++ src/ImageDataExporter.py
@@ -24,86 +24,131 @@ class ImageDataExporter:
         startDate = f"{startYear}-{startMonth}-01"
         endDate = f"{endYear}-{endMonth}-01"
 
+        if startYear == 2016:
+            lastYearStartDate = f"{startYear-1}-07-01"
+            lastYearEndDate = f"{endYear-1}-{endMonth}-01"
+
+            twoYearsAgoStartDate = None
+            twoYearsAgoEndDate = None            
+        else:
+            lastYearStartDate = f"{startYear-1}-{startMonth}-01"
+            lastYearEndDate = f"{endYear-1}-{endMonth}-01"
+
+            if startYear == 2017:
+                twoYearsAgoStartDate = f"{startYear-2}-07-01"
+                twoYearsAgoStartDate = f"{endYear-2}-{endMonth}-01"
+
+            twoYearsAgoStartDate = f"{startYear-2}-{startMonth}-01"
+            twoYearsAgoStartDate = f"{endYear-2}-{endMonth}-01"
+
         self.startDate = startDate
         self.endDate = endDate
 
-    def export_images(self, save_dir, fname_prefix, fname = 'raw_value_counts.csv'):
+        self.lastYearStartDate = lastYearStartDate
+        self.lastYearEndDate = lastYearEndDate
         
-        if DW:
-            if PROB:
-                data = {'tile_no': [i for i in range(self.fh.shape[0])], 'expectation_built': [-1 for _ in range(self.fh.shape[0])]}
-            else:
-                data = {'tile_no': [i for i in range(self.fh.shape[0])], 'perc_built': [-1 for _ in range(self.fh.shape[0])]}
-        else:
-            data = {'tile_no': [i for i in range(self.fh.shape[0])], 'ntl': [-1 for _ in range(self.fh.shape[0])]}
-        # self.df = pd.DataFrame(data)
+        self.twoYearsAgoStartDate = twoYearsAgoStartDate
+        self.twoYearsAgoEndDate = twoYearsAgoEndDate
 
-        if DW:
-            dw = ee.ImageCollection("GOOGLE/DYNAMICWORLD/V1").filter(
-                    ee.Filter.date(self.startDate, self.endDate)
-            )   
+    def export_images(self, save_dir, fname_prefix, fname = 'raw_value_counts.csv'):
+
+        data = {'tile_no': [i for i in range(self.fh.shape[0])], 'expectation_built': [-1 for _ in range(self.fh.shape[0])], \
+                'perc_built': [-1 for _ in range(self.fh.shape[0])], 'ntl': [-1 for _ in range(self.fh.shape[0])], \
+                'perc_new_urban_1year': [-1 for _ in range(self.fh.shape[0])], 'perc_new_urban_2year': [-1 for _ in range(self.fh.shape[0])]}
+
+        dw = ee.ImageCollection("GOOGLE/DYNAMICWORLD/V1").filter(ee.Filter.date(self.startDate, self.endDate))   
+        dw_last_year = ee.ImageCollection("GOOGLE/DYNAMICWORLD/V1").filter(ee.Filter.date(self.lastYearStartDate, self.lastYearEndDate))   
+        if self.twoYearsAgoStartDate:
+            dw_2_years_ago = ee.ImageCollection("GOOGLE/DYNAMICWORLD/V1").filter(ee.Filter.date(self.twoYearsAgoStartDate, self.twoYearsAgoEndDate)) 
         else:
-            viirs = ee.ImageCollection("NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG").filterDate(self.startDate, self.endDate).select('avg_rad')
+            dw_2_years_ago = None
+        viirs = ee.ImageCollection("NOAA/VIIRS/DNB/MONTHLY_V1/VCMSLCFG").filterDate(self.startDate, self.endDate).select('avg_rad')
 
         for i in tqdm(range(self.fh.shape[0])):
 
             try:
                 region =  ee.Geometry.Rectangle(self.fh['geometry'].iloc[i].bounds)
 
-                if DW:
-                    dw_clip = dw.filterBounds(region)
+                dw_clip = dw.filterBounds(region)
 
-                    if PROB:
-                        reducer = ee.Reducer.mean()
+                ### Expectation Built
+                reducer = ee.Reducer.mean()
+                built = dw_clip.select('built')
+                mean_built = built.reduce(reducer)
 
-                        built = dw_clip.select('built')
-                        mean_built = built.reduce(reducer)
+                expected_built = mean_built.reduceRegion(
+                    reducer=ee.Reducer.mean(),
+                    geometry=region,
+                    scale=10
+                ).getInfo()['built_mean']
 
-                        expected_built = mean_built.reduceRegion(
-                            reducer=ee.Reducer.mean(),
-                            geometry=region,
-                            scale=10
-                        ).getInfo()['built_mean']
+                data['expectation_built'][i] = expected_built
 
-                        data['expectation_built'][i] = expected_built
+                ### New Urban
+                dw_clip_last_year = dw_last_year.filterBounds(region)
+                built_last_year = dw_clip_last_year.select('built')
+                mean_built_last_year = built_last_year.reduce(reducer)
+                newUrban1year = mean_built_last_year.lt(0.15).And(mean_built.gt(0.35))
+                data['perc_new_urban_1year'][i] = newUrban1year * 100
 
-                    else:
-                        reducer = ee.Reducer.mode()
+                newUrban1year = newUrban1year.reduceRegion(
+                    reducer=ee.Reducer.mean(),
+                    geometry=region,
+                    scale=10
+                ).getInfo()['built_mean']
 
-                        classification = dw_clip.select('label')
-                        dwComposite = classification.reduce(reducer)
+                if dw_2_years_ago:
+                    dw_clip_2_years_ago = dw_2_years_ago.filterBounds(region)
+                    built_2_years_ago = dw_clip_2_years_ago.select('built')
+                    mean_built_2_years_ago = built_2_years_ago.reduce(reducer)
+                    newUrban2year = mean_built_2_years_ago.lt(0.15).And(mean_built.gt(0.35))
 
-                        builtArea = dwComposite.eq(6)
-
-                        totalPixels = builtArea.reduceRegion(
-                            reducer=ee.Reducer.count(),
-                            geometry=region,
-                            scale=10
-                        ).getInfo()['label_mode']
-
-                        builtAreaMasked = builtArea.selfMask()
-                        builtAreaPixels = builtAreaMasked.reduceRegion(
-                            reducer=ee.Reducer.count(),
-                            geometry=region,
-                            scale=10
-                        ).getInfo()['label_mode']
-
-                        if totalPixels != 0:
-                            percent = builtAreaPixels / totalPixels * 100
-
-                        data['perc_built'][i] = percent
-                else:
-                    viirs_clip = viirs.filterBounds(region)
-                    reducer = ee.Reducer.mean()
-                    viirs_mean = viirs_clip.reduce(reducer)
-
-                    ntl = viirs_mean.reduceRegion(
+                    newUrban2year = newUrban2year.reduceRegion(
                         reducer=ee.Reducer.mean(),
                         geometry=region,
-                        scale=400
-                    ).getInfo()['avg_rad_mean']
+                        scale=10
+                    ).getInfo()['built_mean']
 
-                    data['ntl'][i] = ntl
+                    data['perc_new_urban_2year'][i] = newUrban2year * 100
+
+                ### Proportion Built
+                reducer = ee.Reducer.mode()
+
+                classification = dw_clip.select('label')
+                dwComposite = classification.reduce(reducer)
+
+                builtArea = dwComposite.eq(6)
+
+                totalPixels = builtArea.reduceRegion(
+                    reducer=ee.Reducer.count(),
+                    geometry=region,
+                    scale=10
+                ).getInfo()['label_mode']
+
+                builtAreaMasked = builtArea.selfMask()
+                builtAreaPixels = builtAreaMasked.reduceRegion(
+                    reducer=ee.Reducer.count(),
+                    geometry=region,
+                    scale=10
+                ).getInfo()['label_mode']
+
+                if totalPixels != 0:
+                    percent = builtAreaPixels / totalPixels * 100
+
+                data['perc_built'][i] = percent
+
+                ### NTL
+                viirs_clip = viirs.filterBounds(region)
+                reducer = ee.Reducer.mean()
+                viirs_mean = viirs_clip.reduce(reducer)
+
+                ntl = viirs_mean.reduceRegion(
+                    reducer=ee.Reducer.mean(),
+                    geometry=region,
+                    scale=400
+                ).getInfo()['avg_rad_mean']
+
+                data['ntl'][i] = ntl
 
             except HttpError as e:
                 if e.resp.status == 400 and "Computation timed out" in e._get_reason():
